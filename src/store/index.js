11f3be4425da4715debbeacc2a4540f3d814d4cc
Ebby <tim.eberhardt@gmail.com>
2021-06-25 09:21:53 +0200

    sample store/axios service

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
@@ -7,17 +8,26 @@ export default createStore({
       admin: true,
     },
     navigation: "Home",
+    data: "",
   },
   getters: {
     getSelectedNavItem: state => state.navigation,
     getAuthorizedUser: state => state.authorizedUser,
   },
   mutations: { //setter sync //called via commit('CHANGE_NAVITEM','newNavItem')
-    CHANGE_NAVITEM (state, navItem) {
+    CHANGE_NAVITEM(state, navItem) {
       state.navigation = navItem
+    },
+    DOWNLOAD_DATA(state, data) {
+      state.data = data
     }
   },
   actions: { //setter async only call mutations (could be used for ajax requests) //called via dispatch('function_name','variable')
+    async getData(context) {
+      const { data } = await axios
+        .get("https://api.coindesk.com/v1/bpi/currentprice.json");
+      context.commit("DOWNLOAD_DATA", data)
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
       :tableHeader="tableHeader"
       :tableData="tableData"
     />
-    {{ info }}
+    {{ getData }}
   </div>
 </template>
 
@@ -27,13 +27,16 @@ export default {
         ["21", "22", "23"],
         ["31", "32", "33"],
       ],
-      info: "",
+      info: "Test",
     };
   },
   mounted() {
-    axios
-      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
-      .then((response) => (this.info = response));
+    this.$store.dispatch("getData");
   },
+  computed: {
+    getData() {
+      return this.$store.state.data;
+    }
+  }
 };
 </script>
