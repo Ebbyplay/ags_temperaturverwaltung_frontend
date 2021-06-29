5d44132acd3d6f084a6a57be416c4889f82adb5e
Ebby <tim.eberhardt@gmail.com>
2021-06-29 09:19:33 +0200

    added Sensor Component

---
+++ src/components/SensorComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/css/theme.scss";
+</style>
