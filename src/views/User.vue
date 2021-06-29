a4698f571b0b33185940aab28c40d126a3ba8ccb
Lorenz Amme <Lorenz.Amme@funkwerk.com>
2021-06-29 11:11:39 +0200

    create User.vue

---
+++ src/views/User.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="user">Hello User</div>
+</template>
+
+<script>
+
+export default {
+  name: "User",
+  components: {},
+  data: function data() {
+    return {
+      dataSetName: "TestTabelle",
+    };
+  },
+  mounted() {},
+  methods: {},
+  computed: {},
+};
+</script>
