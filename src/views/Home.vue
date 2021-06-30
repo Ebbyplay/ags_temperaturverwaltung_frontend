d859532c046939c4b4e83fcd1944053997c1e4e7
Ebby <tim.eberhardt@gmail.com>
2021-06-30 15:46:14 +0200

    removed SensorCom from Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,15 @@
 <template>
-  <div class="home">
-    <SensorCom :sensor="sensor" />
-  </div>
+  <div class="home">Home</div>
 </template>
 
 <script>
-import SensorCom from "@/components/SensorCom.vue";
 import axios from "axios";
 
 export default {
   name: "Home",
-  components: {
-    SensorCom,
-  },
+  components: {},
   data: function data() {
-    return {
-      dataSetName: "TestTabelle",
-      sensor: { id: 8, maxTemperature: 60, rackId: 3, manufacturerId: 1 },
-    };
+    return {};
   },
   mounted() {},
   methods: {},
