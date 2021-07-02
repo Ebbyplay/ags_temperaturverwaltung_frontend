36fc8605f59da5d5e979fdf0706856b3a61c9408
Lorenz Amme <Lorenz.Amme@funkwerk.com>
2021-07-02 13:10:08 +0200

    User vue show and edit users

---
+++ src/components/UserCom.vue
@@ -0,0 +1,172 @@
+<template>
+    <div class="container">
+        <div
+        id="header"
+        >
+        <input
+            v-model="id"
+            v-bind:class="[
+              editing ? 'maxTempInputActive' : 'maxTempInputPassive',
+            ]"
+        />
+        <input
+            v-model="name"
+            v-bind:class="[
+              editing ? 'maxTempInputActive' : 'maxTempInputPassive',
+            ]"
+        />
+        <input
+            v-model="nickname"
+            v-bind:class="[
+              editing ? 'maxTempInputActive' : 'maxTempInputPassive',
+            ]"
+        />
+        <input
+            v-model="phonenumber"
+            v-bind:class="[
+              editing ? 'maxTempInputActive' : 'maxTempInputPassive',
+            ]"
+        />
+        <input
+            v-model="admin"
+            v-bind:class="[
+              editing ? 'maxTempInputActive' : 'maxTempInputPassive',
+            ]"
+        />
+        
+        <div id="action-bar">
+
+        <div id="edit-div" v-if="!editing">
+          <i
+            class="edit mdi mdi-pencil" 
+            v-on:click="editing = true"         
+          />
+        </div>
+
+        <div id="edit-actions" v-if="editing">
+          <i
+            class="save mdi mdi-content-save"
+            v-on:click="editing = false"
+          />
+          <i
+            class="cancel mdi mdi-close-thick"
+            v-on:click="editing = false"
+          />
+        </div>
+      </div>
+
+
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: "UserCom",
+  components: {
+  },
+  props: {
+      user: Object,
+  },
+  data: function data() {
+    return {
+      id: this.user.id,
+      name: this.user.name,
+      nickname: this.user.nickname,
+      phonenumber: this.user.phonenumber,
+      admin: this.user.admin,
+      editing: false,
+    };
+  },
+  mounted() {
+  },
+  methods: {
+  },
+  computed: {
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/css/theme.scss";
+.container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: auto;
+  border: solid 1px $bordercolor;
+  border-radius: 8px;
+  box-shadow: 0px 7px 10px 0px darken($backgroundcolor, 5%);
+  font-size: 22px;
+
+  #header {
+    display: flex;
+    width: 100%;
+    height: 50px;
+
+    span {
+      padding: 10px 10px 10px 10px;
+    }
+  }
+
+  #action-bar {
+    display: flex;
+    height: 100%;
+    flex: 1 1 6%;
+    flex-direction: column;
+    text-align: right;
+
+    #edit-div {
+        padding: 10px 5px 10px 5px;
+        min-width: 64px;
+    }
+
+    #edit-actions {
+        padding: 10px 5px 10px 5px;
+        min-width: 64px;
+    }
+
+    .edit,
+    .save,
+    .cancel {
+      cursor: pointer;
+      padding: 10px 5px 10px 5px;
+      &:hover {
+        color: $bordercolor;
+      }
+      &:active {
+        color: darken($bordercolor, 10%);
+      }
+    }
+  }
+
+    .maxTempInputActive {
+        display: inline;
+        font-size: inherit;
+        border: 1px solid $bordercolor;
+        padding: none;
+        background-color: inherit;
+        color: inherit;
+        width: 100%;
+    }
+
+    .maxTempInputPassive {
+        border: none;
+        display: inline;
+        font-size: inherit;
+        padding: none;
+        background-color: inherit;
+        color: inherit;
+        pointer-events: none;
+        width: 100%;
+
+        &:focus {
+            outline: none;
+        }
+    }
+  
+}
+
+
+</style>
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,19 +1,67 @@
 <template>
-  <div class="user">Hello User</div>
+  <h1>Nutzer</h1>
+  <div id="user-container">
+    <UserCom
+      class = "user"
+      v-for="(user, index) in users"
+      :key="index"
+      :user="user"
+    />
+  </div>
 </template>
 
 <script>
 
+import UserCom from "@/components/UserCom.vue";
+
 export default {
   name: "Nutzer",
-  components: {},
+  components: {
+    UserCom
+  },
   data: function data() {
     return {
-      dataSetName: "TestTabelle",
+      users: [],
     };
   },
-  mounted() {},
-  methods: {},
+  mounted() {
+    this.getUsers();
+
+    console.log(this.users);
+  },
+  methods: {
+    getUsers() {
+      this.$store.dispatch("findAll", "user").then(
+        (response) => {
+          for (var i = 0; i < response.length; i++) {
+            var row = response[i];
+            this.users[i] = {
+              id: row.id,
+              name: row.name,
+              nickname: row.nickname,
+              phonenumber: row.phonenumber,
+              admin: row.admin
+            };
+          }
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    },
+  },
   computed: {},
 };
 </script>
+
+<style lang="scss" scoped>
+#user-container {
+  overflow-y: scroll;
+  overflow-x: hidden;
+  height: 90%;
+  padding-right: 20px;
+  .user {
+    margin-top: 20px;
+  }
+}
+</style>
